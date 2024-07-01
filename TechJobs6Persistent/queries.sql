a9765d29862e84838466be853f9122d0b3977a4b
ControlFangs <163154411+ControlFangs@users.noreply.github.com>
2024-07-01 08:54:59 -0500

    done done

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -12,6 +12,8 @@
 -- where Location =St. Louis City
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
-    select
-    innerjoin Jobs = skills
-    and one on the join table we created 
+--   select SkillName
+--     from Skills
+--     Inner Join jobskill ON Skills.Id = SkillsId
+--    ORDER BY SkillName ASC;
+
